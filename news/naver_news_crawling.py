bf1b3ef02bce17669ca545a12e37ecdeb29aaaf4
huni <xogns12356@gmail.com>
2021-01-28 16:00:24 +0900

    add crawling code

---
+++ news/naver_news_crawling.py
@@ -0,0 +1,129 @@
+"""
+기본적으로 이미지 가지고 올 경우 가져온 이미지로 하되
+이미지가 없거나 동영상 같은 경우는 신문사 로고로 대체
+"""
+
+from selenium import webdriver
+
+
+def head_line_news(news_category):
+    li_list = driver.find_elements_by_css_selector(
+        '#today_main_news > div.hdline_news > ul > li')
+
+    for i in range(len(li_list)):
+        li_list = driver.find_elements_by_css_selector(
+            '#today_main_news > div.hdline_news > ul > li')
+
+        url = li_list[i].find_element_by_css_selector(
+            'a').get_attribute('href')
+
+        handle_news(url)
+
+
+def normal_news(news_category, str):
+
+    head_url = news_category.find_element_by_css_selector(
+        'a').get_attribute('href')
+
+    handle_news(head_url)
+
+    li_list = driver.find_elements_by_css_selector(
+        '#section_' + str + ' > div.com_list > div > ul > li')
+
+    for i in range(len(li_list)):
+        li_list = driver.find_elements_by_css_selector(
+            '#today_main_news > div.hdline_news > ul > li')
+
+        url = li_list[i].find_element_by_css_selector(
+            'a').get_attribute('href')
+
+        handle_news(url)
+
+
+def handle_news(url):
+
+    driver.implicitly_wait(5)
+    driver.get(url=url)
+
+    news_title = driver.find_element_by_css_selector(
+        '#articleTitle').text
+
+    news_category = find_category(url)
+
+    newspaper = driver.find_element_by_css_selector(
+        '#articleBody > div.link_news > h3').text
+    newspaper = newspaper.split(' ')[0]
+
+    print("url : " + url + "\nnews_title : " +
+          news_title + "\ncategory : " + news_category + "\nnewspaper : " + newspaper + "\n")
+
+    driver.implicitly_wait(5)
+    driver.get(url=URL_news_home)
+
+
+def find_category(url):
+    if 'sid1=100' in url:
+        return '정치'
+    elif 'sid1=101' in url:
+        return '경제'
+    elif 'sid1=102' in url:
+        return '사회'
+    elif 'sid1=103' in url:
+        return '생활/문화'
+    elif 'sid1=104' in url:
+        return '세계'
+    elif 'sid1=105' in url:
+        return 'IT/과학'
+    else:
+        return '기타'
+
+
+# chrome 창 크기 조절
+options = webdriver.ChromeOptions()
+options.add_argument('window-size=1920,1080')
+
+URL_news_home = 'https://news.naver.com/'
+driver = webdriver.Chrome('chromedriver87', options=options)
+
+# 응답 대기
+driver.implicitly_wait(5)
+
+# news home으로 이동
+driver.get(url=URL_news_home)
+
+# 헤드라인
+head_line = driver.find_elements_by_css_selector(
+    '#today_main_news > div.hdline_news')
+head_line_news(head_line[0])
+
+# 정치
+politics = driver.find_elements_by_css_selector(
+    '#section_politics > div.com_list')
+normal_news(politics[0], 'politics')
+
+# 경제
+economy = driver.find_elements_by_css_selector(
+    '#section_economy > div.com_list')
+normal_news(economy[0], 'economy')
+
+# 사회
+society = driver.find_elements_by_css_selector(
+    '#section_society > div.com_list')
+normal_news(society[0], 'society')
+
+# 생활/문화
+life = driver.find_elements_by_css_selector(
+    '#section_life > div.com_list')
+normal_news(life[0], 'life')
+
+# 세계
+world = driver.find_elements_by_css_selector(
+    '#section_world > div.com_list')
+normal_news(world[0], 'world')
+
+# IT/과학
+it = driver.find_elements_by_css_selector(
+    '#section_it > div.com_list')
+normal_news(it[0], 'it')
+
+driver.close()
---
+++ webtoons/daum_webtoon_crawling.py
@@ -0,0 +1,93 @@
+"""
+1. 로그인(성인 인증)해야 내부 썸네일 크롤링 가능
+    성인 웹툰은 어떻게 처리?
+"""
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+def daum_crawling(li_list, num):
+
+    for i in range(len(li_list)):
+
+        li_list = driver.find_elements_by_css_selector(
+            '#dayList' + str(num) + ' > li')
+
+        webtoon_title = li_list[i].find_element_by_css_selector(
+            'a > img').get_attribute('alt')
+
+        webtoon_url = li_list[i].find_element_by_css_selector(
+            'a').get_attribute('href')
+
+        webtoon_status = li_list[i].find_element_by_css_selector('a > em').text
+
+        webtoon_author = li_list[i].find_element_by_class_name('txt_info').text
+
+        webtoon_thumbnail = li_list[i].find_element_by_css_selector(
+            'a > img').get_attribute('src')
+
+        driver.implicitly_wait(5)
+        driver.get(url=webtoon_url)
+
+        try:
+            webtoon_category = tmp = driver.find_element_by_css_selector(
+                '#cSub > div.product_info > div > div > dl > dd').text
+
+            li_list_webtoon = driver.find_elements_by_css_selector(
+                '#episodeList > ul > li')
+
+            recent_title = ''
+            recent_thumbnail = ''
+            recent_url = ''
+
+            for j in range(len(li_list_webtoon)):
+
+                li_list_webtoon = driver.find_elements_by_css_selector(
+                    '#episodeList > ul > li')
+
+                try:
+                    li_list_webtoon[j].find_element_by_class_name(
+                        'ico_preview')
+                except NoSuchElementException:
+                    recent_thumbnail = li_list_webtoon[j].find_element_by_css_selector(
+                        'a > img').get_attribute('src')
+
+                    recent_title = li_list_webtoon[j].find_element_by_css_selector(
+                        'a > strong').text
+
+                    recent_url = li_list_webtoon[j].find_element_by_css_selector(
+                        'a').get_attribute('href')
+                    break
+
+            print("제목 : " + webtoon_title +
+                  "\n작가 : " + webtoon_author +
+                  "\n카테고리 : " + webtoon_category +
+                  "\n업데이트 상태 : " + webtoon_status +
+                  "\n썸네일 : " + webtoon_thumbnail +
+                  "\nURL : " + webtoon_url +
+                  "\n최신 작품 제목 : " + recent_title +
+                  "\n최신 작품 썸네일 : " + recent_thumbnail +
+                  "\n최신 작품 URL : " + recent_url + "\n")
+        except:
+            continue
+        finally:
+            driver.implicitly_wait(5)
+            driver.get(url=URL_webtoon_home)
+
+
+# chrome 창 크기 조절
+options = webdriver.ChromeOptions()
+options.add_argument('window-size=1920,1080')
+
+URL_webtoon_home = 'http://webtoon.daum.net/'
+driver = webdriver.Chrome('chromedriver87', options=options)
+
+driver.implicitly_wait(5)
+driver.get(url=URL_webtoon_home)
+
+li_list_up = driver.find_elements_by_css_selector('#dayList1 > li')
+li_list_down = driver.find_elements_by_css_selector('#dayList2 > li')
+
+daum_crawling(li_list_up, 1)
+daum_crawling(li_list_down, 2)
