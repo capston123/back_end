ffa712128346a1c438307ebff08d38372bc6363b
litmuss <sunsu2737@gmail.com>
2021-04-08 16:40:26 +0900

    history version0.1

--- contents/urls.py
+++ contents/urls.py
@@ -15,6 +15,17 @@ urlpatterns = [
     path('naverwebtoon/', webtoon_views.naverwebtoon),
     path('navernews/', news_views.news),
     path('history/', history_views.hisotry),
+
+    path('recommend_nt/', history_views.recomend_naver),
+    path('recommend_dt/', history_views.recomend_daum),
+    path('recommend_yt/', history_views.recomend_youtube),
+    path('recommend_news/', history_views.recomend_news),
+    
+    path('history_dt/', history_views.history_daum),
+    path('history_nt/', history_views.history_naver),
+    path('history_news/', history_views.history_news),
+    path('history_yt/', history_views.history_youtube),
+
     path('youtube/',you_views.youtube),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
